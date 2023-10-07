9eb61418216ce2b4324edef69ce98a07b1dec826
Tyuiu.BotterVV@outlook.com <masya2012a@gmail.com>
2023-10-07 20:37:19 +0500

    Task5

---
+++ Tyuiu.BotterVV.Sprint1.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.BotterVV.Sprint1.Task5.V5.Lib
+{
+    public class DataService : ISprint1Task5V5
+    {
+        public int Calculate(double x)
+        {
+            double d = (x % 1) * 10;
+            var res = Convert.ToInt32(d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BotterVV.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BotterVV.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.BotterVV.Sprint1.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Боттер В.В. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Боттер Вадим Валерьевич | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу: Присвоить целой    *");
+            Console.WriteLine("* переменной d первую цифру из дробной части положительного вещественного *");
+            Console.WriteLine("* числа x                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите дробное число: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadKey();
+        }
+    }
+}
