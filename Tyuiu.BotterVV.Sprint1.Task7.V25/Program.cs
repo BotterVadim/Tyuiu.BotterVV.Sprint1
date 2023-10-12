a5afdf3c41d5ff515ac9ee97941a878d29426c8b
Tyuiu.BotterVV@outlook.com <masya2012a@gmail.com>
2023-10-12 09:44:33 +0500

    Task7

---
+++ Tyuiu.BotterVV.Sprint1.Task7.V25/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BotterVV.Sprint1.Task7.V25.Lib;
+
+namespace Tyuiu.BotterVV.Sprint1.Task7.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Боттер В.В. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итоговых проектов по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Боттер Вадим Валерьевич | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*           y^2 + 6 + cosx^3 + xy - 2x^2                                  *");
+            Console.WriteLine("* z = e^x - ----------------------------                                  *");
+            Console.WriteLine("*              sin(x^4 + 13) + 9y - 2                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите x : ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y : ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
